77df6412008e16829ddd03a052d717aa435ebce0
jacobstac <jacob.benjamin.stachowicz@gmail.com>
2020-03-06 17:30:30 +0100

    made a initial plan for a way of implementation

---
+++ TSP_Engine/main.js
@@ -0,0 +1,13 @@
+/*
+Initial plan: 
+1. Make this file read the dataset
+2. Make a distance matrix
+2.1 (optional or later) MAKE ORDERED ADJENCENCY LISTS FOR EACH CITY
+3. make a initial tour (gready or random)
+4. Make tour improvement algorithms for example:
+4.1 localRandom
+4.2 Two opt
+4.3 Two point Five opt
+5. find a way to print the results
+
+*/
