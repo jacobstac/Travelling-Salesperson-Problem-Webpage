3ea16a45c2524f276a7516a797b8b787e53f6520
jacobstac <jacob.benjamin.stachowicz@gmail.com>
2020-03-13 16:12:22 +0100

    create array of nodes, and a functionality so they can be added to the grid

--- TSP_Engine/grid.js
+++ TSP_Engine/grid.js
@@ -1,26 +1,46 @@
 console.log("det funkar");
 
-
 var table = document.querySelector("table");
 
 const width = 20;
-const height = 10;
+const height = 30;
 
-
-
-function createGrid() {
-    for(i = 0; i < width; i++) {
-        var tr = document.createElement("tr");
-        for(j = 0; j < height; j++) {
-            var th = document.createElement("th");
-            th.appendChild(document.createTextNode("+"));
-            tr.appendChild(th);
-        }
-        table.appendChild(tr);
-    }
+class node {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 }
 
+function createGrid() {
+  for (i = 0; i < width; i++) {
+    var tr = document.createElement("tr");
+    for (j = 0; j < height; j++) {
+      var th = document.createElement("th");
+      th.appendChild(document.createTextNode("+"));
+      tr.appendChild(th);
+    }
+    table.appendChild(tr);
+  }
+}
 
-
-
+var cities = [];
 createGrid();
+
+var cells = document.querySelectorAll("th");
+Array.from(cells).forEach(function(cell) {
+  cell.addEventListener("click", function(event) {
+    var marked = event.target;
+    var cell = marked.cellIndex;
+    var row = marked.parentElement.rowIndex;
+    marked.innerHTML = "\u2B24"
+    marked.className = "marked"
+
+
+
+    cities.push(new node(cell, row));
+
+    console.log("cell is: " + cell);
+    console.log("row is: " + row);
+  });
+});
