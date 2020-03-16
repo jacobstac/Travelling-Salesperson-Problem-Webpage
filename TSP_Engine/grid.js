3feff3406dc107f67466131ace729dc2c67dcfb2
jacobstac <jacob.benjamin.stachowicz@gmail.com>
2020-03-16 18:30:41 +0100

    clean funtion code to to ES6 standard

--- TSP_Engine/grid.js
+++ TSP_Engine/grid.js
@@ -12,7 +12,7 @@ class node {
   }
 }
 
-function createGrid() {
+const createGrid = () => {
   for (i = 0; i < width; i++) {
     var tr = document.createElement("tr");
     for (j = 0; j < height; j++) {
@@ -24,28 +24,46 @@ function createGrid() {
   }
 }
 
-function nodeListen(){
-    var cells = document.querySelectorAll("th");
-    Array.from(cells).forEach(function(cell) {
-      cell.addEventListener("click", function(event) {
-        var marked = event.target;
-        var cell = marked.cellIndex;
-        var row = marked.parentElement.rowIndex;
-        marked.innerHTML = "\u2B24"
-        marked.className = "marked"
-        cities.push(new node(cell, row));
-    
-        console.log("cell is: " + cell);
-        console.log("row is: " + row);
-      });
-    });
+const addNode = (event) => {
+    var marked = event.target;
+    var cell = marked.cellIndex;
+    var row = marked.parentElement.rowIndex;
+    marked.innerHTML = "\u2B24"
+    marked.className = "marked"
+    cities.push(new node(cell, row));
+
+    console.log("cell is: " + cell);
+    console.log("row is: " + row);
 }
 
-
-
+const nodeListen = () => {
+    var cells = document.querySelectorAll("th");
+    Array.from(cells).forEach(function(cell) { cell.addEventListener("click", addNode);});
+}
 
 var cities = [];
 createGrid();
 nodeListen();
 
+
+var n = document.querySelectorAll("th");
+var n1 = n[1];
+
+var rect = n1.getBoundingClientRect();
+var y1 = (rect.bottom + rect.top) / 2;
+var x1 = (rect.right + rect.left) / 2;
+
+console.log(y1, x1)
+
+
+
+
+// 2. Make a distance matrix
+// 2.1 (optional or later) MAKE ORDERED ADJENCENCY LISTS FOR EACH CITY
+// 3. make a initial tour (gready or random)
+// 4. Make tour improvement algorithms for example:
+// 4.1 localRandom
+// 4.2 Two opt
+// 4.3 Two point Five opt
+// 5. find a way to print the results
 
